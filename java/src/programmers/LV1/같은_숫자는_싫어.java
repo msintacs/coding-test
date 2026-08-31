a1dbb60ba7726c4d830d927a2c5397c83c9e2c6f
msintacs <ksbthyls@gmail.com>
2026-08-31 13:37:24 +0900

    프로그래머스 Lv1 같은 숫자는 싫어 (Java)

---
+++ java/src/programmers/LV1/같은_숫자는_싫어.java
@@ -0,0 +1,42 @@
+package programmers.LV1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class 같은_숫자는_싫어 {
+
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        int[] arr1 = {1, 1, 3, 3, 0, 1, 1};
+        int[] arr2 = {4, 4, 4, 3, 3};
+
+        int[] result1 = solution.solution(arr1);
+        int[] result2 = solution.solution(arr2);
+
+        System.out.println(Arrays.toString(result1));
+        System.out.println(Arrays.toString(result2));
+    }
+
+    static class Solution {
+        public int[] solution(int[] arr) {
+
+            List<Integer> answer = new ArrayList<>();
+            answer.add(arr[0]);
+
+            for (int i=1; i<arr.length; i++) {
+                int prev = arr[i - 1];
+
+                if (prev != arr[i]) {
+                    answer.add(arr[i]);
+                }
+            }
+
+            return answer.stream()
+                    .mapToInt(Integer::intValue)
+                    .toArray();
+        }
+    }
+}
